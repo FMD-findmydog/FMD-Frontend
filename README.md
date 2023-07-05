8586aae4e10f60cb0e748ea3d283d35b50e27a62
jjeongsu <jeongsu.dev@gmail.com>
2023-07-05 14:26:03 +0900

    test commit

---
+++ README.md
@@ -0,0 +1,5 @@
+## Feature Login Page
+
+홈페이지 로그인, 소셜 로그인 기능 담담
+author : 이정수
+
---
+++ src/pages/index.tsx
@@ -0,0 +1,16 @@
+
+import { Inter } from 'next/font/google'
+import tw, { css, styled } from 'twin.macro'
+
+const inter = Inter({ subsets: ['latin'] })
+const Hello = styled.div([
+  tw`text-center`,
+  css`
+    color: red;
+  `
+])
+export default function Home() {
+  return (
+    <Hello> hello</Hello>
+  )
+}
