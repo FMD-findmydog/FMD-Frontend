1a25c5f79b50c9603bf9a46d6a8b980d27c88743
Ariling <97501226+Ariling@users.noreply.github.com>
2023-07-22 17:14:32 +0900

    사진 다중 업로드 기능 구현 (삭제나 수정 제외)

--- src/components/utils/Photo.tsx
+++ src/components/utils/Photo.tsx
@@ -1,7 +1,68 @@
-import React from "react";
+import Image from "next/image";
+import React, { useState } from "react";
 
 const Photo = () => {
-  return <div>사진 슬라이드로 담을 부분</div>;
+  //이거는 나중에 인터페이스로 만들예정!
+  const [uploadImages, setUploadImages] = useState<{
+    imageFiles: File[];
+    imageUrls: string[];
+  }>({
+    imageFiles: [],
+    imageUrls: [],
+  });
+  const handleImageChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (!e.target.files) return;
+    else if (e.target.files.length > 4) {
+      alert("사진을 너무 넣지 마세요!");
+      return;
+    }
+    const files = e.target.files;
+    const fileArray = Array.from(files);
+
+    const newImages = Array.from(files, (file) => URL.createObjectURL(file));
+    setUploadImages({
+      imageFiles: [...uploadImages.imageFiles, ...fileArray],
+      imageUrls: [...uploadImages.imageUrls, ...newImages],
+    });
+  };
+  const handleDeleteImage = (id: number) => {
+    setUploadImages({
+      imageFiles: uploadImages.imageFiles.filter((_, index) => index !== id),
+      imageUrls: uploadImages.imageUrls.filter((_, index) => index !== id),
+    });
+  };
+  return (
+    <div>
+      <div>
+        {uploadImages.imageUrls.length === 0 ? (
+          <div>대표이미지삽입</div>
+        ) : (
+          <Image
+            src={uploadImages.imageUrls[0]}
+            alt="대표이미지"
+            className=" w-40 h-40"
+          />
+        )}
+      </div>
+      <input
+        type="file"
+        id="inputFile"
+        accept="image/*"
+        multiple
+        onChange={handleImageChange}
+      />
+      <div className="flex">
+        {uploadImages.imageUrls.map((url, idx) => (
+          <>
+            <div key={idx}>
+              <Image src={url} className="w-20 h-20" alt={`${url} - ${idx}`} />
+            </div>
+            <button>삭제하기</button>
+          </>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Photo;
